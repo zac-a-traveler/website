5887c7932394cba3dce25af31bb8b411693e4ab1
Mehrshad <mehrshadfiroozian@gmail.com>
2019-12-10 23:15:22 +0330

    do some neccessary changes to the project such as login and index page

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,3 @@
 from django import forms
+from django.contrib.auth.models import User
+
--- main/models.py
+++ main/models.py
@@ -5,4 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profession = models.CharField(max_length=20) # to specify the profession of the user 
                                                  # for instance SysAdmin, Programmer and etc.
-    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return self.user.first_name
