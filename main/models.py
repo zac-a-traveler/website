3898695b97f314bf1180ff4451dd3fb89bcf0d53
Mehrshad <mehrshadfiroozian@gmail.com>
2019-12-10 23:03:01 +0330

    Prevent from an unwanted bug

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profession = models.CharField(max_length=20) # to specify the profession of the user 
                                                  # for instance SysAdmin, Programmer and etc.
     profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
